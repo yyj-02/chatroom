cea1b91c85b4cc6970350014c0426f9bc6f9410c
yyj-02 <yeohyongjie@outlook.com>
2024-04-07 11:15:38 +0800

    Add chats

--- chatbot-app/src/pages/Chatroom.tsx
+++ chatbot-app/src/pages/Chatroom.tsx
@@ -7,12 +7,20 @@ import { ArrowLeft, Send } from "lucide-react";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Input } from "@/components/ui/input";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
 
 const Chatroom = () => {
   const { id } = useParams();
   const userId = 1;
 
-  const [britishMode, setBritishMode] = useState(true);
+  const [britishMode, setBritishMode] = useState(false);
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
   const [chats, setChats] = useState<Chats>([
@@ -37,6 +45,15 @@ const Chatroom = () => {
       },
     },
   ]);
+
+  const getInitials = (name: string) => {
+    return name
+      .split(" ")
+      .slice(0, 2)
+      .map((s) => s[0])
+      .join("")
+      .toUpperCase();
+  };
 
   const addChat = (chat: Chat) => {
     setChats([...chats, chat]);
@@ -66,13 +83,14 @@ const Chatroom = () => {
         name: "User 1",
       },
     });
+
     setLoading(false);
     setMessage("");
   };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     // enter and no shift enter
-    if (event.key === "Enter" && !event.shiftKey) {
+    if (event.key === "Enter") {
       event.preventDefault();
       handleMessageSend();
     }
@@ -105,7 +123,61 @@ const Chatroom = () => {
         <div className="hidden flex-shrink flex-grow basis-28 xl:block" />
       </div>
       <div className="mx-auto flex w-full max-w-5xl grow flex-col gap-2 px-8 pb-8">
-        <ScrollArea className="w-full grow rounded-md border bg-white/5 p-4"></ScrollArea>
+        <ScrollArea className="w-full grow rounded-md border bg-white/5 px-4">
+          {chats.map((chat) => {
+            const isUser = chat.user.id === userId;
+            return (
+              <div className="py-4">
+                <div
+                  className={cn(
+                    "flex items-start gap-2",
+                    isUser && "flex-row-reverse",
+                  )}
+                >
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <Avatar>
+                          <AvatarFallback>
+                            {getInitials(chat.user.name)}
+                          </AvatarFallback>
+                        </Avatar>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <span className="text-xs">{chat.user.name}</span>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <div
+                          className={cn(
+                            "min-h-10 max-w-96 shrink rounded-md bg-secondary/70 p-2 leading-7",
+                            isUser && "bg-secondary",
+                          )}
+                        >
+                          {britishMode
+                            ? chat.translatedMessage
+                            : chat.originalMessage}
+                        </div>
+                      </TooltipTrigger>
+                      <TooltipContent
+                        className="border-none bg-black/0 px-2 py-0 text-white/50"
+                        side="bottom"
+                        align={isUser ? "end" : "start"}
+                      >
+                        <span className="text-xs leading-none">
+                          {chat.timestamp}
+                        </span>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollArea>
         <div className="flex gap-2">
           <Input
             placeholder="Type a message"
